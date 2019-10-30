200448a7f530c2475f4deea285e5dc63902668e8
edsoncarlos33@hotmail.com <edsoncarlos33@hotmail.com>
2019-10-29 21:50:15 -0300

    Adiciona campo de filtro para as imagens

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,82 @@
 <template>
 <div>
-<h1>{{titulo}}</h1>
-  <ul>
-    <li v-for='foto of fotos'>
-      <img :src='foto.url' :alt='foto.titulo'>
-    </li>
-  </ul>
-</div>
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue' 
+
 export default {
+
+  components: {
+
+    'meu-painel': Painel
+  },
+
   data () {
-    
     return {
-      titulo: 'Alurapic',
-      fotos: []
+      titulo: 'Alurapic', 
+      fotos: [],
+      filtro: ''
     }
   },
 
+   computed: {
+
+  fotosComFiltro() {
+      if (this.filtro) {
+          // criando uma expressão com o valor do filtro, insensitivo
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        // retorna apenas as fotos que condizem com a expressão
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+
+    }
+  },
   created() {
-    
-    this.$http.get('http://localhost:3000/v1/fotos') /*realiza requisição http para API*/
-    .then(res => res.json()) /*converte o resultado para json*/
-    .then(foto => this.fotos = foto); /*atribui o resultado para o objeto fotos[]*/
+
+    this.$http
+      .get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
 
 <style>
+.centralizado {
+    text-align: center;
+  }
 
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
 
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+  .imagem-responsiva {
+    width:100%;
+  }
+
+  .filtro {
+    width: 40%;
+  }
 </style>
