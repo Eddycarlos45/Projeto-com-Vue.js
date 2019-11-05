f4ef31b2e563b5fb0fb24123f41bd12dc0ca8670
edsoncarlos33@hotmail.com <edsoncarlos33@hotmail.com>
2019-11-04 21:29:08 -0300

    Adiciona componente botao

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -9,6 +9,7 @@
         <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
             <meu-painel :titulo="foto.titulo">
             <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+            <meu-botao rotulo="remover" tipo="button"/>
             </meu-painel>
         </li>
         </ul>
@@ -18,14 +19,16 @@
 <script>
 
 import Painel from '../shared/painel/Painel.vue';
-import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Botao from '../shared/botao/Botao.vue'
 
 export default {
 
   components: {
 
     'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
   },
 
   data () {
